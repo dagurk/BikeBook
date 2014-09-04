3c542f421fb7247d069440059d9871dfc07bd6eb
Dagur Kristinsson <dagurk@gmail.com>
2014-09-04 18:56:21 +0000

    Bike

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,43 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('LandingPageController', [function() {
 
   }])
-  .controller('MyCtrl2', [function() {
+  .controller('BikeController', ['$scope', 'bikeService', 'authService', function($scope, bikeService, authService) {
 
-  }]);
+  	// Bind bikes to $scope.bikes.
+  	authService.getCurrentUser().then(function(user) {
+  		if(user) {
+  			$scope.bikes = bikeService.getBikes(user.id);
+  		};
+  	});
+
+  	// Object to store data from the bike form.
+  	$scope.newBike = {name: '', color: '', size: '', verified: false};
+
+  	// Function to save a new bike.
+  	$scope.saveBike = function() {
+  		bikeService.saveBike($scope.newBike, $scope.currentUser.id);
+  		// Reset the newBike variable
+      $scope.newBike = {name: '', color: '', size: '', verified: false};
+  	};
+
+  }])
+	.controller('AuthController', ['$scope', 'authService', function($scope, authService) {
+
+		// Objcet bond to inputs on the register and login pages.
+		$scope.user = {email: '', password: ''};
+
+		$scope.register = function() {
+			authService.register($scope.user);
+		};
+
+		$scope.login = function() {
+			authService.login($scope.user);
+		};
+		$scope.logout = function() {
+			authService.logout();
+		};
+
+	}]);
--- app/js/services.js
+++ app/js/services.js
@@ -2,8 +2,79 @@
 
 /* Services */
 
+angular.module('myApp.services', [])
+	.value('FIREBASE_URL', 'https://bikebook.firebaseIO.com/')
+	.factory('dataService', function($firebase, FIREBASE_URL) {
+		var dataRef = new Firebase(FIREBASE_URL);
+		var fireData = $firebase(dataRef);
 
-// Demonstrate how to register services
-// In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+		return fireData;
+	})
+	.factory('bikeService', function(dataService) {
+  	var bikes = dataService.$child('bikes');
+
+  	var bikeServiceObject = {
+  		saveBike: function(bike, userId) {
+  			bikes.$add(bike);
+  		},
+  		getBikes: function(userId) {
+  			return bikes;
+  		}
+  	};
+	
+		return bikeServiceObject;
+	})
+	.factory('textMessageService', function(dataService, partyService) {
+		var textMessages = dataService.$child('textMessages');
+
+		var textMessageServiceObject = {
+			sendTextMessage: function(party, userId) {
+				var newTextMessage = {
+					phonenumber: party.phone,
+					size: party.size,
+					name: party.name
+				};
+				textMessages.$add(newTextMessage);
+				partyService.getPartiesByUserId(userId).$child(party.$id).$update({notified: 'Yes'});
+			}
+		};
+
+		return textMessageServiceObject;
+	})
+	.factory('authService', function($firebaseSimpleLogin, $location, FIREBASE_URL, $rootScope, dataService) {
+		var authRef = new Firebase(FIREBASE_URL);
+		var auth = $firebaseSimpleLogin(authRef);
+		var emails = dataService.$child('emails');
+
+		var authServiceObject = {
+			register: function(user) {
+				auth.$createUser(user.email, user.password).then(function(data) {
+					console.log(data);
+					emails.$add({email: user.email});
+					authServiceObject.login(user);
+				});
+			},
+			login: function(user) {
+				auth.$login('password', user).then(function(data) {
+					console.log(data);
+					$location.path('/bikes');
+				});
+			},
+			logout: function() {
+				auth.$logout();
+				$location.path('/');
+			},
+			getCurrentUser: function() {
+				return auth.$getCurrentUser();
+			}
+		};
+
+		$rootScope.$on("$firebaseSimpleLogin:login", function(e, user) {
+		  $rootScope.currentUser = user;
+		});
+		$rootScope.$on("$firebaseSimpleLogin:logout", function() {
+		  $rootScope.currentUser = null;
+		});
+
+		return authServiceObject;
+	});
